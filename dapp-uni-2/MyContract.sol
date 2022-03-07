491cb2c600198db8af1c5bc9f09a563e51a98541
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2022-03-08 00:22:05 +1100

    struct and enum in solidity

--- dapp-uni-2/MyContract.sol
+++ dapp-uni-2/MyContract.sol
@@ -1,10 +1,31 @@
 pragma solidity 0.5.1;
 
 contract MyContract {
-    string value;
+    string public value;
+    string public value2 = "myValue2";
+    string public constant value3 = "constantValue";
+    bool public myBool = true;
+    int public myInt = -1;
+    uint public myUint = 1;
+    uint public myUnit8 = 8;
+    uint256 public myUint256 = 88888;
+
+    enum State { Waiting, Ready, Active } 
+    State public state;
+
+    //Person[] public people;
+    mapping (uint => Person) public people;
+    uint256 public peopleCount;
+
+    struct Person {
+        uint256 _id;
+        string _firstname;
+        string _lastname;
+    }
 
     constructor() public {
         value = "myValue";
+        state = State.Waiting;
     }
 
     function get() public view returns(string memory) {
@@ -13,5 +34,19 @@ contract MyContract {
 
     function set(string memory _value) public {
         value = _value;
+    }
+
+    function activate() public {
+        state = State.Active;
+    }
+
+    function isActive() public view returns(bool) {
+        return state == State.Active;
+    }
+
+    function addPerson(string memory _firstname, string memory _lastname) public {
+        //people.push(Person(_firstname, _lastname));
+        peopleCount += 1;
+        people[peopleCount] = Person(peopleCount, _firstname, _lastname);
     }
 }
